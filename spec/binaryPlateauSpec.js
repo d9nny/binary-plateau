2a68351ad6b8957bded6bb2c8d165d519e803621
danwheeler <da-wheeler@hotmail.co.uk>
2016-06-16 09:40:41 +0100

    project set up and basic function/ test set up

---
+++ spec/binaryPlateauSpec.js
@@ -0,0 +1,6 @@
+describe('binaryPlateau', function() {
+    it('should find the largest binary plateau when given an integer', function() {
+      var result = binaryPlateau( );
+      expect(result).toEqual( );
+    });
+});
---
+++ src/binaryPlateau.js
@@ -0,0 +1,13 @@
+var binaryPlateau = function(number) {
+
+	function convertToBinary() {
+		
+	}
+
+	function findLargestPlateau() {
+
+	}
+
+	return findLargestPlateau(convertToBinary(number));
+	
+};
